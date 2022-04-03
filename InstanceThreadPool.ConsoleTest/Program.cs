059ee66568c475f2d24dd909fa2da2f4568023a4
Eugene <chepurko0@gmail.com>
2022-04-03 17:05:57 +0300

    Add project files.

---
+++ InstanceThreadPool.ConsoleTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace InstanceThreadPool.ConsoleTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadLine();
+        }
+    }
+}
